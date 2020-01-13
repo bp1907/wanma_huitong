08c34033cd002c833b1b0aff45440f3690000a0c
GsminGitHub <1500969875@qq.com>
2020-01-13 15:36:15 +0800

    flutter升级配置

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,10 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/xxzx/Desktop/mywork/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/xxzx/wanma_huitong"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/xxzx/wanma_huitong/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/xxzx/Desktop/mywork/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
